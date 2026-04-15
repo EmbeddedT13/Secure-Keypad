934cb327793bf0e93a79a9a03202bb69644f6d26
AhmedAbdelsalam2004 <a3abdelsalam2005@gmail.com>
2026-04-15 04:32:19 +0200

    feat: implement NVIC memory map - Mapped NVIC core registers (ISER0, ISER1, IPR) with strict volatile types - Defined core peripheral base address for NVIC processor interrupts - Created abstraction macros for EXTI IRQ mappings and Priority levels - Declared public API function prototypes for NVIC manipulation

---
+++ mcal/nvic.h
@@ -0,0 +1,33 @@
+#ifndef NVIC_H
+#define NVIC_H
+
+#include "../core/BIT_MATH.h"
+#include "../core/STD_TYPES.h"
+
+/* NVIC Base Address */
+#define NVIC_BASE_ADDRESS 0xE000E100
+
+/* Core Interrupt Set-Enable Registers */
+#define NVIC_ISER0     *((volatile uint32*)(NVIC_BASE_ADDRESS + 0x000)) /* Enable external interrupts from 0 to 31  */
+#define NVIC_ISER1     *((volatile uint32*)(NVIC_BASE_ADDRESS + 0x004)) /* Enable external interrupts from 32 to 63 */
+
+
+#define NVIC_IPR        ((volatile uint8*) (NVIC_BASE_ADDRESS + 0x300)) /* Interrupt Priority Registers (Byte-accessible) */
+
+/* Priority Levels */
+#define NVIC_PRIORITY_URGENT  0
+#define NVIC_PRIORITY_NORMAL  1
+
+/* EXTI IRQ Number Mappings */
+#define NVIC_IRQ_EXTI0      6
+#define NVIC_IRQ_EXTI1      7
+#define NVIC_IRQ_EXTI2      8
+#define NVIC_IRQ_EXTI3      9
+#define NVIC_IRQ_EXTI4      10
+#define NVIC_IRQ_EXTI9_5    23
+#define NVIC_IRQ_EXTI15_10  40
+
+void NVIC_EnableInterrupt(uint8 IRQ_Num);
+void NVIC_SetPriority(uint8 IRQ_Num, uint8 Priority);
+
+#endif
