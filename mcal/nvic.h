08cc1edd3beec5862b8c1f1d3fc980c7e068c837
AhmedAbdelsalam2004 <a3abdelsalam2005@gmail.com>
2026-04-15 04:47:06 +0200

    feat: add interrupt disable functionality to NVIC - Mapped core Interrupt Clear-Enable Registers (ICER0, ICER1) with strict volatile types - Declared public API function prototype for NVIC interrupt disable manipulation

--- mcal/nvic.h
+++ mcal/nvic.h
@@ -10,6 +10,8 @@
 /* Core Interrupt Set-Enable Registers */
 #define NVIC_ISER0     *((volatile uint32*)(NVIC_BASE_ADDRESS + 0x000)) /* Enable external interrupts from 0 to 31  */
 #define NVIC_ISER1     *((volatile uint32*)(NVIC_BASE_ADDRESS + 0x004)) /* Enable external interrupts from 32 to 63 */
+#define NVIC_ICER0     *((volatile uint32*)(NVIC_BASE_ADDRESS + 0x080)) /* Disable external interrupts from 0 to 31 */
+#define NVIC_ICER1     *((volatile uint32*)(NVIC_BASE_ADDRESS + 0x084)) /* Disable external interrupts from 32 to 63 */
 
 
 #define NVIC_IPR        ((volatile uint8*) (NVIC_BASE_ADDRESS + 0x300)) /* Interrupt Priority Registers (Byte-accessible) */
@@ -28,6 +30,7 @@
 #define NVIC_IRQ_EXTI15_10  40
 
 void NVIC_EnableInterrupt(uint8 IRQ_Num);
+void NVIC_DisableInterrupt(uint8 IRQ_Num);
 void NVIC_SetPriority(uint8 IRQ_Num, uint8 Priority);
 
 #endif
